4124bcaad4241c305a4ca376bc733929b9e2742d
Russ Gibson <russg@rnstech.com>
2021-08-23 14:17:18 -0700

    Switching branchs to try out tutbo.js handling.

--- nl/customers/popups/handlers.py
+++ nl/customers/popups/handlers.py
@@ -1,9 +1,12 @@
 
+from datetime import datetime, date, timezone
+
 from flask import request, flash, url_for
 from flask_security import login_required
 
 from nl import db
 from nl.customers.popups import bp
+from nl.utils import flash_success
 
 
 @bp.route('/create_payment', methods=('POST',))
@@ -11,21 +14,21 @@ from nl.customers.popups import bp
 def create_payment():
     """
     """
-    import requests
+
+    # p = Payment()
+    # p.customer_id = 
+    # p.period_id Config.get()
+    # p.created = updated = datetime.now(timezone.utc)
+    # p.type = form.data['type']
+    # p.date = date.now(timezone.utc)
+    # p.amount = form.data['amount']
+    # p.extra1 = form.data['id']
+    # p.extra2 = None
+    # p.tip = float(form.data['tip'])
+    # p.note = form.data['notes']
     
-    data = request.get_json()
-    ajax = dict(
-        customer=data['payment-cid'],
-        type=data['payment-type'],
-        amount=data['payment-amount'],
-        tip=data['payment-tip'],
-        notes=data['payment-notes'],
-    )
     
-    r = requests.post(url_for('api.customers.payments.create', _external=True), ajax)
-    r.raise_for_status()
-    
-    flash_success(f'Added payment of {amount} to customer {customer_id}')
+    flash_success(f'Added payment of {p.amount} to customer {p.customer_id}')
 
     return '', 204
 
